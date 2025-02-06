115eda7b20d8620e872829befa5da89081dfb8bb
Christian Birchler <birchler.chr@gmail.com>
2025-02-06 16:14:27 +0100

    define counter interface

--- main.go
+++ main.go
@@ -5,19 +5,40 @@ import (
 	"net/http"
 )
 
-type EchoHandler struct {
+type Counter interface {
+	increment() (int, error)
+	reset() (int, error)
 }
 
-func (h *EchoHandler)ServeHTTP(rw http.ResponseWriter, r *http.Request){
-	slog.Info("echo handler", "request", r, "response", rw)
+type PostgresCounter struct {
+}
+
+func (pc *PostgresCounter) increment() (int, error) {
+	// TODO
+	return 0, nil
+}
+
+func (pc *PostgresCounter) reset() (int, error) {
+	// TODO
+	return 0, nil
+}
+
+type PingHandler struct {
+	counter Counter
+}
+
+func (h *PingHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	slog.Debug("echo handler", "request", r, "response", rw)
+	h.counter.increment()
 	rw.Write([]byte("pong"))
 }
-
 
 func main() {
 	slog.Info("start go-sample")
 
-	http.Handle("/ping", &EchoHandler{})
+	http.Handle("/ping", &PingHandler{
+		counter: &PostgresCounter{},
+	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		slog.Error("server failed", "error", err)
